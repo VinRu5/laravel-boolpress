89c775ea9509dbfd54a32c88e22d7b4fc3776d0c
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-09-13 16:54:24 +0200

    stampa dati

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $posts = Post::all();
+        
+
+        return view('home', compact('posts'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,58 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        @if (session('status'))
         <div class="col-md-8">
             <div class="card">
+
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                <div class="card-body">
+
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                 </div>
             </div>
         </div>
+        @endif
+
+        @foreach($posts as $post)
+
+        <div class="col-md-8">
+            <div class="card">
+
+                <div class="card-header">
+                    <div class="name-user">Filippo Rossi</div>
+                    <!--todo: dinamicizzare-->
+                    <div class="date">{{ $post->created_at }}</div>
+                </div>
+
+
+                <div class="card-body">
+
+                    <div class="post">
+
+                        <div class="post-text">
+                            {{ $post->text }}
+                        </div>
+
+                        @if(strlen($post->photo) !== 0)
+                        <div class="post-img">
+                            <img src="{{ $post->photo }}" alt="photo">
+                        </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+
+
+
+
     </div>
+
 </div>
 @endsection

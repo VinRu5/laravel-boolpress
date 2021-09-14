b1960f7f3e153d47e90caa77f92f60337b9edce5
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-09-14 17:13:27 +0200

    page index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,34 +2,31 @@
 
 @section('content')
 
-@foreach($posts as $post)
+<div class="container">
 
-<div class="col-md-8">
-    <div class="card">
-
-        <div class="card-header">
-            <div class="name-user">{{ Auth::user()->name }}</div>
-            <div class="date">{{ $post->created_at }}</div>
-        </div>
-
-
-        <div class="card-body">
-
-            <div class="post">
-
-                <div class="post-text">
-                    {{ $post->text }}
-                </div>
-
-                @if(strlen($post->photo) !== 0)
-                <div class="post-img">
-                    <img src="{{ $post->photo }}" alt="photo">
-                </div>
-                @endif
+    <div class="row card-header">
+        <div scope="col-10">Posts</div>
+        <div scope="col-2"><button class="btn btn-primary">New</button></div>
+    </div>
+    @foreach($posts as $post)
+    <div class="row card-body">
+        <div class="col-1">{{$post->id}}</div>
+        <div class="col-8">
+            <div class="tilte-post">
+                {{$post->title_post}}
+            </div>
+            <div class="info-post">
+                {{ $post->created_at }}
             </div>
         </div>
+        @if(strlen($post->photo) !== 0)
+        <div class="col-2 col-lg-1"><img src="{{$post->photo}}" alt="picture of {{$post->title}}" /></div>
+        @endif
+        <div class="col-1"><a href=""><i class="fas fa-ellipsis-h"></i></a></div>
     </div>
+    @endforeach
 </div>
-@endforeach
+
+
 
 @endsection
